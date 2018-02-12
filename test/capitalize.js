fc0e573b9801ff4c6bb644dfa42b61b4b4ac6a69
Xiaoyun <yangx232@gmail.com>
2018-02-11 21:54:12 -0500

    fixed eslint errors in test files

--- test/capitalize.js
+++ test/capitalize.js
@@ -1,40 +1,41 @@
-var capitalize = require("../server/modules/capitalize")
-var chai = require("chai")
-var expect = chai.expect;
+const capitalize = require('../server/modules/capitalize');
+const chai = require('chai');
 
-describe("capitalize", function() {
-  it("capitalizes single words", function() {
-    expect(capitalize("express")).to.equal("Express");
-    expect(capitalize("cats")).to.equal("Cats");
-  })
-  it("makes the rest of the string lowercase", function() {
-    expect(capitalize("javaScript")).to.equal("Javascript")
-  })
-   it("leaves empty strings alone", function() {
-    expect(capitalize("")).to.equal("");
-  })
-  it("leaves strings with no words alone", function() {
-    expect(capitalize("  ")).to.equal("  ");
-    expect(capitalize("123")).to.equal("123");
-  })
-  it("capitalizes multiple-word strings", function() {
-    expect(capitalize("what is Express?")).to.equal("What is express?");
-    expect(capitalize("i love lamp")).to.equal("I love lamp");
-  })
-  it("leaves already-capitalized words alone", function() {
-    expect(capitalize("Express")).to.equal("Express");
-    expect(capitalize("Evan")).to.equal("Evan");
-    expect(capitalize("Catman")).to.equal("Catman");
-  })
-  it("capitalizes String objects without changing their values", function() {
-    var str = new String("who is JavaScript?");
-    expect(capitalize(str)).to.equal("Who is javascript?");
-    expect(str.valueOf()).to.equal("who is JavaScript?");
-  })
-  it("throws an error if passed a number", function() {
-    expect(function() { capitalize(123); }).to.throw(Error);
-  })
-  it("changes the value", function() {
-    expect(capitalize("foo")).not.to.equal("foo");
-  })
-})
+const expect = chai.expect;
+
+describe('capitalize', () => {
+  it('capitalizes single words', () => {
+    expect(capitalize('express')).to.equal('Express');
+    expect(capitalize('cats')).to.equal('Cats');
+  });
+  it('makes the rest of the string lowercase', () => {
+    expect(capitalize('javaScript')).to.equal('Javascript');
+  });
+  it('leaves empty strings alone', () => {
+    expect(capitalize('')).to.equal('');
+  });
+  it('leaves strings with no words alone', () => {
+    expect(capitalize('  ')).to.equal('  ');
+    expect(capitalize('123')).to.equal('123');
+  });
+  it('capitalizes multiple-word strings', () => {
+    expect(capitalize('what is Express?')).to.equal('What is express?');
+    expect(capitalize('i love lamp')).to.equal('I love lamp');
+  });
+  it('leaves already-capitalized words alone', () => {
+    expect(capitalize('Express')).to.equal('Express');
+    expect(capitalize('Evan')).to.equal('Evan');
+    expect(capitalize('Catman')).to.equal('Catman');
+  });
+  it('capitalizes String objects without changing their values', () => {
+    const str = 'who is JavaScript?';
+    expect(capitalize(str)).to.equal('Who is javascript?');
+    expect(str.valueOf()).to.equal('who is JavaScript?');
+  });
+  it('throws an error if passed a number', () => {
+    expect(() => { capitalize(123); }).to.throw(Error);
+  });
+  it('changes the value', () => {
+    expect(capitalize('foo')).not.to.equal('foo');
+  });
+});
--- test/html.js
+++ test/html.js
@@ -1,32 +1,32 @@
-var app = require("../server/server")
-var supertest = require("supertest");
-var cheerio = require("cheerio");
+const app = require('../server/server');
+const supertest = require('supertest');
+const cheerio = require('cheerio');
 
-describe("html response", function() {
-  var request;
-  beforeEach(function() {
+describe('html response', () => {
+  let request;
+  beforeEach(() => {
     request = supertest(app) // SuperTest builds up the request.
-      .get("/test/")  // You visit the "/test" URL.
-      .set("User-Agent", "a cool browser")
-      .set("Accept", "text/html") // Sets a header describing what content type we want back from the server
-  })
-  it("returns an HTML response", function(done) {
-     request
-      .expect("Content-Type", /html/)
+      .get('/test/') // You visit the "/test" URL.
+      .set('User-Agent', 'a cool browser')
+      .set('Accept', 'text/html'); // Sets a header describing what content type we want back from the server
+  });
+  it('returns an HTML response', (done) => {
+    request
+      .expect('Content-Type', /html/)
       .expect(200)
       .end(done);
-  })
-  it("returns your User Agent", function(done) {
-     request
-      .expect(function(res) {
-        var htmlResponse = res.text;
-        var $ = cheerio.load(htmlResponse);
-        var userAgent = $(".user-agent").html().trim();
-        if (userAgent !== "a cool browser") {
-          throw new Error("User Agent not found");
+  });
+  it('returns your User Agent', (done) => {
+    request
+      .expect((res) => {
+        const htmlResponse = res.text;
+        const $ = cheerio.load(htmlResponse);
+        const userAgent = $('.user-agent').html().trim();
+        if (userAgent !== 'a cool browser') {
+          throw new Error('User Agent not found');
         }
       })
       .end(done);
-  })
+  });
 // ...
-})
+});
