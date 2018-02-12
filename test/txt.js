13232735c89e107cc5f1597379c1e13bbb7c78e6
Xiaoyun <yangx232@gmail.com>
2018-02-11 21:57:32 -0500

    fixed eslint errors in test files

--- test/txt.js
+++ test/txt.js
@@ -1,28 +1,28 @@
-var app = require("../server/server")
-var supertest = require("supertest")
+const app = require('../server/server');
+const supertest = require('supertest');
 
-describe("plain text response", function() {
-  var request;
-  beforeEach(function() {
+describe('plain text response', () => {
+  let request;
+  beforeEach(() => {
     request = supertest(app) // SuperTest builds up the request.
-      .get("/test/")  // You visit the "/test" URL.
-      .set("User-Agent", "my cool browser")
-      .set("Accept", "text/plain") // Sets a header describing what content type we want back from the server
-  })
-  it("returns a plain text response", function(done) {
-     request
-      .expect("Content-Type", /text\/plain/)
+      .get('/test/') // You visit the "/test" URL.
+      .set('User-Agent', 'my cool browser')
+      .set('Accept', 'text/plain'); // Sets a header describing what content type we want back from the server
+  });
+  it('returns a plain text response', (done) => {
+    request
+      .expect('Content-Type', /text\/plain/)
       .expect(200)
       .end(done);
-  })
-  it("returns your User Agent", function(done) {
-     request
-      .expect(function(res) {
-        if (res.text !== "my cool browser") {
-          throw new Error("Response does not contain User Agent");
+  });
+  it('returns your User Agent', (done) => {
+    request
+      .expect((res) => {
+        if (res.text !== 'my cool browser') {
+          throw new Error('Response does not contain User Agent');
         }
       })
       .end(done);
-  })
+  });
 // ...
-})
+});
