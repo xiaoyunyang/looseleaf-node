2049c49afe4c7ba9686706e0988de6f4ddae56c1
Xiaoyun <yangx232@gmail.com>
2018-05-24 23:05:03 -0400

    updated how it works md

--- client/src/shared/Guest/HowItWorks.js
+++ client/src/shared/Guest/HowItWorks.js
@@ -7,20 +7,7 @@ export default ( {route} ) => (
     <TopNav route={route} />
     <div className="container main">
       <div>
-        <h5>Markdown Stuff: </h5>
         <MdText filepath='http://localhost:3001/how-it-works.md' mdId='how-it-works'/>
-        <h5>Non-Markdown Stuff: </h5>
-        <h3>How It Works</h3>
-        <h3>How we provide value for</h3>
-
-        <h4>Non-profits:</h4>
-        <p>
-          LooseLeaf helps nonprofits find volunteers to work on app development,
-        </p>
-        <p>{'Let aspiring web developers help you build a website for your non-profit.'}</p>
-          <h4>For Employers:</h4>
-          <p>{'Grittiness is an indicator of success. Find Gritty prospective employers here.'}</p>
-        <p>{'.'}</p>
       </div>
     </div>
   </div>
