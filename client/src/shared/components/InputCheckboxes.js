ca9301b77a55b9e16acb7aaedcb31ced163da595
Xiaoyun <yangx232@gmail.com>
2018-07-25 23:12:14 -0400

    added input checkbox component

---
+++ client/src/shared/components/InputCheckboxes.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class InputCheckboxes extends React.Component {
+  static defaultProps = {
+    units: '',
+    label: '',
+    title: ''
+  }
+
+  handleClick(d, event) {
+    const clicked = d;
+    const isChecked = event.target.checked;
+
+    let newArr = null;
+    if (isChecked) {
+      newArr = this.props.selected.concat(clicked);
+    } else {
+      newArr = this.props.selected.filter(d => d !== clicked);
+    }
+    this.props.handleChange(newArr);
+  }
+  renderUnits(data, units) {
+    if (typeof data === 'number') {
+      return data > 1 ? `${units}s` : units;
+    }
+    return units;
+  }
+  renderCheckbox(d) {
+    return (
+      <p key={`check-${this.props.d}`} onChange={this.handleClick(d)} >
+        <input type="checkbox" id={d} checked="checked" />
+        <label htmlFor={d}>
+          {`${d} ${this.renderUnits(d, this.props.units)}`}
+        </label>
+      </p>
+    );
+  }
+  render() {
+    return (
+      <div>
+        <p>{this.props.title}</p>
+        {
+          this.props.choices.map(d => {
+            const checked = this.props.selected.includes(d) ? 'checked' : ''
+            return (
+              <p key={`check-${d}`} >
+                <input type="checkbox" id={`${this.props.id}-${d}`} onChange={this.handleClick.bind(this, d)} checked={checked} />
+                <label htmlFor={`${this.props.id}-${d}`}>
+                  {`${d} ${this.renderUnits(d, this.props.units)}`}
+                </label>
+              </p>
+            );
+          })
+        }
+      </div>
+    );
+  }
+}
+
+
+InputCheckboxes.propTypes = {
+  id: PropTypes.string.isRequired,
+  choices: PropTypes.array.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  units: PropTypes.string,
+  label: PropTypes.string,
+  title: PropTypes.string
+};
--- client/src/shared/components/InputDropdown.js
+++ client/src/shared/components/InputDropdown.js
@@ -1,38 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import $ from 'jquery';
 
 export default class InputDropDown extends React.Component {
-
+  static defaultProps = {
+    units: '',
+    label: '',
+    title: ''
+  }
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick;
+  }
   componentDidMount() {
-    $('#'+this.props.id).material_select(
+    $(`#${this.props.id}`).material_select(
       this.handleClick.bind(this)
     );
   }
   handleClick() {
-    let newData = $('#'+this.props.id).val();
-    this.props.setState(newData)
+    const newData = $(`#${this.props.id}`).val();
+    this.props.setState(newData);
   }
   renderUnits(data, units) {
-    if(!units) return '';
-
-    if (typeof(data) === 'number') {
-      return data > 1 ? units + 's' : units;
+    if (typeof data === 'number') {
+      return data > 1 ? `${units}s` : units;
     }
     return units;
   }
 
   render() {
     return (
-      <div style={{paddingTop: '12px'}} className='input-field col l6 m6 s6'>
+      <div style={{ paddingTop: '12px' }} className="input-field col l6 m6 s6">
+        <p>{this.props.title}</p>
         <select id={this.props.id} defaultValue='None'>
-          <option disabled>{this.props.label}</option>
+          <option value="" disabled selected>
+            {this.props.label ? this.props.label : 'Choose your option'}
+          </option>
           {
-            this.props.choices.map(function(d,i) {
-              return (
-                <option value={d} key={i}>
-                  {d + ' ' + this.renderUnits(d, this.props.units)}
-                </option>);
-            }.bind(this))
+            this.props.choices.map(d => (
+              <option value={d} key={`dropdown-${d}`}>
+                {`${d} ${this.renderUnits(d, this.props.units)}`}
+              </option>
+            ))
           }
         </select>
         <label>{this.props.label}</label>
@@ -40,3 +49,11 @@ export default class InputDropDown extends React.Component {
     );
   }
 }
+InputDropDown.propTypes = {
+  id: PropTypes.string.isRequired,
+  choices: PropTypes.array.isRequired,
+  setState: PropTypes.func.isRequired,
+  units: PropTypes.string,
+  label: PropTypes.string,
+  title: PropTypes.string
+};
