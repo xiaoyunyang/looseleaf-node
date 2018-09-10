ace6b28dc962c7f34259c26460ad090098ae875f
Xiaoyun <yangx232@gmail.com>
2018-09-09 20:28:48 -0400

    user deleting post from own profile page updates posts appropriately

--- client/src/shared/components/Portfolio/Two.js
+++ client/src/shared/components/Portfolio/Two.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { getApiData } from '../../../lib/helpers';
 import { apiLink } from '../../data/apiLinks';
 import Posts from '../Collection/Posts';
+import Discussion from '../Discussion';
 
 export default class Two extends React.Component {
   constructor(props) {
@@ -18,6 +19,12 @@ export default class Two extends React.Component {
     const setApiData = data => this.setState({ posts: data });
     getApiData(url, setApiData);
   }
+  deletePost(postId) {
+    const newPosts = this.state.posts.filter(d => {
+      return d._id !== postId;
+    });
+    this.setState({ posts: newPosts });
+  }
   render() {
     return (
       <div className="container">
@@ -27,6 +34,7 @@ export default class Two extends React.Component {
             posts={this.state.posts}
             loggedInAs={this.props.loggedinAs}
             noPostDisp="This user has no posts."
+            deletePost={this.deletePost.bind(this)}
           />
         </div>
       </div>
